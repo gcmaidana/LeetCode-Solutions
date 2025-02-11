70a3a1518fb3072675830e18ca6e895ea825d11f
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-02-11 16:08:13 -0600

    Update 053-Product-of-Array-Except-Self-Medium.py

--- grind75/week1/053-Product-of-Array-Except-Self-Medium/053-Product-of-Array-Except-Self-Medium.py
+++ grind75/week1/053-Product-of-Array-Except-Self-Medium/053-Product-of-Array-Except-Self-Medium.py
@@ -1,22 +1,24 @@
 # Question link: https://leetcode.com/problems/product-of-array-except-self/description/
 
-# It would be easy to create a prefix and postfix array to do this problem. The tricky part of this problem is using O(1) space (the output array doesn't count as extra space though)
-# The idea is to use the answer array to do the prefix, work backwards and compute the result using postfix, and the prefix values in the array
-
+# Basically everything to left of an index and everything to the right of an index multiplied together gives the correct answer for a given index
+# To simulate this, we maintain an ans array to multiple everything to the left of a given index in nums and then loop back about in reverse
+# for everything to the right of an index in nums, but all of this calculation is stored in ans
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = [1] * len(nums)
+        ans = [1] * len(nums)
         prefix = 1
+
         for i in range(len(nums)):
-            res[i] = prefix
-            prefix *= nums[i]
+            ans[i] = ans[i] * prefix
+            prefix = nums[i] * prefix
+        
         postfix = 1
         for i in reversed(range(len(nums))):
-            res[i] *= postfix  
-            postfix *= nums[i]
+            ans[i] = ans[i] * postfix
+            postfix = nums[i] * postfix
+        
 
-        return res
-
-
+        return ans
+        
 
         

3edd5d407fc2101632e8e8b20118c050d168d64b
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-07 19:53:48 -0600

    Update 001-Contains-Duplicate-Easy.py

--- neetcode-roadmap/arrays/001-Contains-Duplicate-Easy.py
+++ neetcode-roadmap/arrays/001-Contains-Duplicate-Easy.py
@@ -10,4 +10,15 @@ class Solution:
                 s.add(n)
         
         return False
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+
+        nums.sort()
+
+        for i in range(1, len(nums)):
+            if nums[i] == nums[i - 1]:
+                return True
+        return False
         

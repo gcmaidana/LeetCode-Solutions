2d41d135fdc9f2213f3a3e932acb40a1a0cf391e
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-09 21:15:09 -0500

    Update 004-Group-Anagrams-Medium.py

--- neetcode-roadmap/arrays/004-Group-Anagrams-Medium/004-Group-Anagrams-Medium.py
+++ neetcode-roadmap/arrays/004-Group-Anagrams-Medium/004-Group-Anagrams-Medium.py
@@ -1,1 +1,24 @@
 # Problem link: https://leetcode.com/problems/group-anagrams/description/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        sortMap = {}
+        ans = []
+        
+        for word in strs:
+            sorted_key = ''.join(sorted(word)) # sorted(word) is a list of characters, so must join them
+            if sorted_key not in sortMap:
+                sortMap[sorted_key] = [word] # store word in a list (not just a str)
+            else:
+                sortMap[sorted_key].append(word)
+        
+        # group like this:
+        # eat => a e t
+        # tea => a e t
+        # etc
+
+        for k, v in sortMap.items():
+            ans.append(v)
+
+        return ans
+        

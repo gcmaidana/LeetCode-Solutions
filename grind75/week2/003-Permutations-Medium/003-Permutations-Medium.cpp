2ab3a111a187968b94823d2d39994f56daed1804
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-11 14:34:15 -0500

    Update 003-Permutations-Medium.cpp

--- grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
+++ grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
@@ -3,28 +3,41 @@
 // Least optimal solution but helps you understand what to do
 class Solution {
 public:
-    std::vector<std::vector<int>> permute(std::vector<int>& nums) {
-        std::vector<std::vector<int>> ans;
-        std::vector<int> perm_arr;
+    vector<vector<int>> permute(vector<int>& nums) {
 
-        // Backtracking function
-        std::function<void()> bt = [&]() {
-            if (perm_arr.size() == nums.size()) {
-                ans.push_back(perm_arr);
+        if(nums.size() == 1)
+        {
+            return {nums};
+        }
+
+        
+
+        std::vector<std::vector<int>> ans;
+        std::vector<int> permArr;
+
+        std::function<void()> bt = [&]()
+        {
+            if(permArr.size() == nums.size())
+            {
+                ans.push_back(permArr);
                 return;
             }
 
-            for (int x : nums) {
-                // Check if x is already in perm_arr
-                if (std::find(perm_arr.begin(), perm_arr.end(), x) == perm_arr.end()) {
-                    perm_arr.push_back(x);
-                    bt();  // Recurse
-                    perm_arr.pop_back();  // Backtrack
+            for(int x : nums)
+            {
+                // is x in permutation array? if not, add it!
+                if(std::find(permArr.begin(), permArr.end(), x) == permArr.end())
+                {
+                    permArr.push_back(x);
+                    bt();
+                    permArr.pop_back();
                 }
             }
         };
 
-        bt();  // Start the backtracking process
+        
+
+        bt();
         return ans;
     }
 };

2abc7603f1a3acdbf553350f95a964dfbcd0e223
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-11 14:35:20 -0500

    Update 003-Permutations-Medium.cpp

--- grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
+++ grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
@@ -41,3 +41,42 @@ public:
         return ans;
     }
 };
+
+
+// Slightly more optimal uses a set for faster lookup
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        // Base case: If nums has only one element, return it as a single permutation
+        if (nums.size() == 1) {
+            return {nums}; // Return a vector of vectors containing the single element
+        }
+
+        vector<vector<int>> ans; // To store all permutations
+        unordered_set<int> used; // To track used elements in the current permutation
+
+        // Helper function for backtracking
+        function<void(vector<int>&)> bt = [&](vector<int>& perm_arr) {
+            // If the current permutation array has the same size as nums, add it to ans
+            if (perm_arr.size() == nums.size()) {
+                ans.push_back(perm_arr);
+                return;
+            }
+
+            for (int x : nums) {
+                if (used.find(x) == used.end()) { // If x is not yet used
+                    used.insert(x); // Mark x as used
+                    perm_arr.push_back(x); // Add x to the current permutation
+                    bt(perm_arr); // Recurse with the updated permutation
+                    perm_arr.pop_back(); // Backtrack: remove the last element
+                    used.erase(x); // Unmark x as used
+                }
+            }
+        };
+
+        vector<int> perm_arr; // Temporary array to build permutations
+        bt(perm_arr); // Start backtracking with an empty array
+
+        return ans; // Return all permutations
+    }
+};

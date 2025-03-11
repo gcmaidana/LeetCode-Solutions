2854033c1c5788169ad20e9c2872a88ecb970df3
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-11 14:41:51 -0500

    Update 003-Permutations-Medium.cpp

--- grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
+++ grind75/week2/003-Permutations-Medium/003-Permutations-Medium.cpp
@@ -46,9 +46,11 @@ public:
 // Slightly more optimal uses a set for faster lookup
 class Solution {
 public:
-    vector<vector<int>> permute(vector<int>& nums) {
+    vector<vector<int>> permute(vector<int>& nums) 
+{
         // Base case: If nums has only one element, return it as a single permutation
-        if (nums.size() == 1) {
+        if (nums.size() == 1) 
+        {
             return {nums}; // Return a vector of vectors containing the single element
         }
 
@@ -56,26 +58,29 @@ public:
         unordered_set<int> used; // To track used elements in the current permutation
 
         // Helper function for backtracking
-        function<void(vector<int>&)> bt = [&](vector<int>& perm_arr) {
+        function<void(vector<int>&)> bt = [&](vector<int>& permArr) 
+    {
             // If the current permutation array has the same size as nums, add it to ans
-            if (perm_arr.size() == nums.size()) {
-                ans.push_back(perm_arr);
+            if (permArr.size() == nums.size()) 
+            {
+                ans.push_back(permArr);
                 return;
             }
 
             for (int x : nums) {
-                if (used.find(x) == used.end()) { // If x is not yet used
+                if (used.find(x) == used.end()) 
+                { // If x is not yet used
                     used.insert(x); // Mark x as used
-                    perm_arr.push_back(x); // Add x to the current permutation
-                    bt(perm_arr); // Recurse with the updated permutation
-                    perm_arr.pop_back(); // Backtrack: remove the last element
+                    permArr.push_back(x); // Add x to the current permutation
+                    bt(permArr); // Recurse with the updated permutation
+                    permArr.pop_back(); // Backtrack: remove the last element
                     used.erase(x); // Unmark x as used
                 }
             }
         };
 
-        vector<int> perm_arr; // Temporary array to build permutations
-        bt(perm_arr); // Start backtracking with an empty array
+        vector<int> permArr; // Temporary array to build permutations
+        bt(permArr); // Start backtracking with an empty array
 
         return ans; // Return all permutations
     }

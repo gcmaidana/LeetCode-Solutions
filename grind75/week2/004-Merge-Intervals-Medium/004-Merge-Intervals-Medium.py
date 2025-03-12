96d66943b4327b33d8f79178fbffaca4194955ad
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-03-11 19:36:32 -0500

    Update 004-Merge-Intervals-Medium.py

--- grind75/week2/004-Merge-Intervals-Medium/004-Merge-Intervals-Medium.py
+++ grind75/week2/004-Merge-Intervals-Medium/004-Merge-Intervals-Medium.py
@@ -3,15 +3,40 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-        intervals.sort(key = lambda i  : i[0])
-        output = [intervals[0]]
+        # output is a list of list
+        # what to do if overlap point on point, e.g. 2, and 2
 
-        for start, end in intervals[1:]:
-            lastEnd = output[-1][1] # end value of most recent interval
+        # fucking disgusting problem can have unsorted start times
+        # like [[1,3], [0,5]]
+        intervals.sort()
 
-            if start <= lastEnd:
-                output[-1][1] = max(lastEnd, end)
+        start = 0
+        end = 1
+        ans = []
+        prev = intervals[0]
+        for interval in intervals[1:]:
+
+            if interval[start] <= prev[end]:
+                prev[end] = max(interval[end], prev[end])
             else:
-                output.append([start,end])
+                ans.append(prev)
+                prev = interval
         
-        return output
+        # Either we merge and don't append quite yet
+        # or append but set the last one and not append it 
+        # in either case, we have an interval to append
+        ans.append(prev)
+
+        
+        return ans
+
+        '''
+    1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18
+    .___.
+      ._______.
+                  .____.
+                                      ._________.
+        '''
+        
+
+      

6b7f5c7989f3880fddc015b3c4622ff973e769b7
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-04-04 16:03:35 -0500

    Update 006-Time-Based-Key-Value-Store-Medium.py
    Updated notes // comments

--- grind75/week2/006-Time-Based-Key-Value-Store-Medium/006-Time-Based-Key-Value-Store-Medium.py
+++ grind75/week2/006-Time-Based-Key-Value-Store-Medium/006-Time-Based-Key-Value-Store-Medium.py
@@ -1,34 +1,44 @@
 # Problem link: https://leetcode.com/problems/time-based-key-value-store/description/
 
 class TimeMap:
+    # NOTE: All the timestamps timestamp of set are strictly increasing.
+
 
     def __init__(self):
-        self.store = {} # key : list of [val, timestamp]
-
+        self.hmap = {} # key : list of [val, timestamp]
+        
+    # set the key. for the values, set the value and timestamp together 
+    # gets added to end of the list so it's O(1)
     def set(self, key: str, value: str, timestamp: int) -> None:
-        if key not in self.store:
-            self.store[key] = []
-        self.store[key].append([value, timestamp])
+        if key not in self.hmap:
+            self.hmap[key] = []
+        self.hmap[key].append([value, timestamp])
         
 
-        
-
+    # keep in mind we have a pair, but we are returning only the value (str)
+    # if we dont find a match, return the most recent val given the key
+    # why do we use binary search? because its better than a linear scan
+    # O(logN) is BETTER than O(n)
     def get(self, key: str, timestamp: int) -> str:
         ans = ""
-        values = self.store.get(key, []) # if not found return empty list
+        values = self.hmap.get(key, []) # if no key found, return empty list
 
-        # binary search
-        l, r = 0, len(values) - 1
+        l = 0
+        r = len(values) - 1
         while l <= r:
             m = (l + r) // 2
-            if values[m][1] <= timestamp:
-                ans = values[m][0]
+
+            # okay here, what we do is:
+            # we want to see if we have match still so we move the pointer
+            # to the right, however, this value is still valid and could be
+            # the most recent so we update ans first
+            if values[m][1] <= timestamp: # check midpoint timestamp
+                ans = values[m][0] # set value on ans
                 l = m + 1
             else:
                 r = m - 1
-        
-        return ans
 
+        return ans
         
 
 

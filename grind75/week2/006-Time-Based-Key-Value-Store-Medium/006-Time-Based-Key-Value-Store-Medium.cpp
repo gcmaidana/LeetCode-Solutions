0e59e53c3c753f1917336682b0c8d19ed6769604
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-04-04 17:06:41 -0500

    Update 006-Time-Based-Key-Value-Store-Medium.cpp

--- grind75/week2/006-Time-Based-Key-Value-Store-Medium/006-Time-Based-Key-Value-Store-Medium.cpp
+++ grind75/week2/006-Time-Based-Key-Value-Store-Medium/006-Time-Based-Key-Value-Store-Medium.cpp
@@ -1,42 +1,48 @@
 // Problem link: https://leetcode.com/problems/time-based-key-value-store/description/
 
-#include <string>
-#include <unordered_map>
-#include <vector>
-using namespace std;
 
 class TimeMap {
+    // NOTE: All the timestamps timestamp of set are strictly increasing.
+
 private:
-    // key -> vector of pairs of (value, timestamp)
-    unordered_map<string, vector<pair<string, int>>> hmap;
+    unordered_map<string, vector<pair<string, int>>> hmap; // key : list of [val, timestamp]
 
 public:
-    TimeMap() 
-    {
-        // Constructor
+    TimeMap() {
+        // constructor
     }
 
-
-    void set(string key, string value, int timestamp) 
-    {
-        hmap[key].emplace_back(value, timestamp); // O(1) append
+    // set the key. for the values, set the value and timestamp together 
+    // gets added to end of the list so it's O(1)
+    void set(string key, string value, int timestamp) {
+        if (hmap.find(key) == hmap.end()) {
+            hmap[key] = vector<pair<string, int>>();
+        }
+        hmap[key].push_back({value, timestamp});
     }
 
-    string get(string key, int timestamp)     
-{
+    // keep in mind we have a pair, but we are returning only the value (str)
+    // if we dont find a match, return the most recent val given the key
+    // why do we use binary search? because its better than a linear scan
+    // O(logN) is BETTER than O(n)
+    string get(string key, int timestamp) {
         string ans = "";
-        auto& values = hmap[key]; // Get the vector of (value, timestamp) pairs
+        vector<pair<string, int>>& values = hmap[key]; // if no key found, return empty list
 
-        int l = 0, r = values.size() - 1;
+        int l = 0;
+        int r = values.size() - 1;
+
         while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (values[m].second <= timestamp) 
-            {
-                ans = values[m].first;
+            int m = (l + r) / 2;
+
+            // okay here, what we do is:
+            // we want to see if we have match still so we move the pointer
+            // to the right, however, this value is still valid and could be
+            // the most recent so we update ans first
+            if (values[m].second <= timestamp) { // check midpoint timestamp
+                ans = values[m].first; // set value on ans
                 l = m + 1;
-            } 
-            else 
-            {
+            } else {
                 r = m - 1;
             }
         }

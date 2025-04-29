baddb3783c9cce3e702da98f74a94c72b7136c41
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-04-29 12:08:23 -0500

    Update 008-Sort-Colors-Medium.cpp
    typo

--- grind75/week2/008-Sort-Colors-Medium/008-Sort-Colors-Medium.cpp
+++ grind75/week2/008-Sort-Colors-Medium/008-Sort-Colors-Medium.cpp
@@ -4,7 +4,7 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int zero =0, one = 0, two = 0;
+        int zero = 0, one = 0, two = 0;
         for(int i = 0; i < nums.size(); i++)
         {
             if(nums[i] == 0) { zero += 1; }

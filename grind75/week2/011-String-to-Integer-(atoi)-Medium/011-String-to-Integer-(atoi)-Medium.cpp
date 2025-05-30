cc63529b76c530cd39dd577e07da404980a3b998
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-05-30 14:49:04 -0500

    Update 011-String-to-Integer-(atoi)-Medium.cpp

--- grind75/week2/011-String-to-Integer-(atoi)-Medium/011-String-to-Integer-(atoi)-Medium.cpp
+++ grind75/week2/011-String-to-Integer-(atoi)-Medium/011-String-to-Integer-(atoi)-Medium.cpp
@@ -1,1 +1,42 @@
-// Placeholder for now
+// Problem link: https://leetcode.com/problems/string-to-integer-atoi/
+
+class Solution {
+public:
+    int myAtoi(string s) {
+        int i = 0;
+        int n = s.length();
+        
+        // Strip leading whitespace
+        while (i < n && s[i] == ' ') 
+        {
+            i++;
+        }
+
+        // Handle sign
+        int sign = 1;
+        if (i < n && s[i] == '-') 
+        {
+            sign = -1;
+            i++;
+        } 
+        else if (i < n && s[i] == '+') 
+        {
+            i++;
+        }
+
+        // Handle digits
+        long result = 0; // use long to detect overflow before clamping
+        while (i < n && isdigit(s[i])) 
+        {
+            result = result * 10 + (s[i] - '0');
+
+            // Clamp if overflow detected
+            if (sign == 1 && result > INT_MAX) return INT_MAX;
+            if (sign == -1 && -result < INT_MIN) return INT_MIN;
+
+            i++;
+        }
+
+        return sign * result;
+    }
+};

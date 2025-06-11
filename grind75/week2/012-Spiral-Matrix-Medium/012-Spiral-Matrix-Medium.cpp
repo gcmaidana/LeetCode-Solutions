11496e23e1ffdce2d38cd15e0443b8ceb2716a9f
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2025-06-11 16:14:34 -0500

    Update 012-Spiral-Matrix-Medium.cpp

--- grind75/week2/012-Spiral-Matrix-Medium/012-Spiral-Matrix-Medium.cpp
+++ grind75/week2/012-Spiral-Matrix-Medium/012-Spiral-Matrix-Medium.cpp
@@ -1,1 +1,51 @@
-// Placeholder
+// problem link: https://leetcode.com/problems/spiral-matrix/
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        vector<int> ans;
+        int ROWS = matrix.size();
+        int COLS = matrix[0].size();
+
+        int top = 0, bottom = ROWS - 1;
+        int left = 0, right = COLS - 1;
+
+        while (top <= bottom && left <= right)
+        {
+            // move left to right
+            for(int i = left; i <= right; i++)
+            {
+                ans.push_back(matrix[top][i]);
+            }
+            top++;
+
+            // move top to bottom
+            for(int i = top; i <= bottom; i++)
+            {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+            
+            // right to left
+            if(top <= bottom)
+            {
+                for(int i = right; i >= left; i--)
+                {
+                    ans.push_back(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+
+            // bottom to top
+            if(left <= right)
+            {
+                for(int i = bottom; i >= top; i--)
+                {
+                    ans.push_back(matrix[i][left]);
+                }
+                left++; 
+            }
+        }
+        
+        return ans;
+    }
+};

a063adb2efa56091214084193f79b0c3aeb6ea6f
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-03-19 20:02:46 -0500

    Update 019-Letter-Combinations-of-a-Phone-Number.cpp

--- grind75/week2/019-Letter-Combinations-of-a-Phone-Number/019-Letter-Combinations-of-a-Phone-Number.cpp
+++ grind75/week2/019-Letter-Combinations-of-a-Phone-Number/019-Letter-Combinations-of-a-Phone-Number.cpp
@@ -1,1 +1,38 @@
+// link: https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+class Solution {
+private:
+    void backtrack(int index, string currentString, 
+        string& digits, unordered_map<char, string>& digits_map, vector<string>&ans)
+    {
+        if (currentString.size() == digits.size())
+        {
+            ans.push_back(currentString);
+            return;
+        } 
 
+        string letters = digits_map[digits[index]];
+        for(char letter : letters)
+        {
+            backtrack(index + 1, currentString + letter,
+                      digits, digits_map, ans);
+        }
+    }
+
+
+public:
+    vector<string> letterCombinations(string digits) 
+    {
+        unordered_map<char, string> digits_map = 
+        {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
+        };
+        vector<string> ans;
+
+
+        backtrack(0, "", digits, digits_map, ans);
+        return ans;
+
+    }
+};

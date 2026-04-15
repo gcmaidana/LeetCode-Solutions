b2818e2ec58697b4f8158767323dd767a001b3b4
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-04-15 12:39:39 -0500

    Update 024-LRU-Cache-Medium.cpp

--- grind75/week2/024-LRU-Cache-Medium/024-LRU-Cache-Medium.cpp
+++ grind75/week2/024-LRU-Cache-Medium/024-LRU-Cache-Medium.cpp
@@ -1,1 +1,105 @@
 // https://leetcode.com/problems/lru-cache/description/
+
+class Node {
+public:
+    int key;
+    int val;
+    Node* prev;
+    Node* next;
+
+    Node(int k, int v) {
+        key = k;
+        val = v;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+
+class LRUCache {
+public:
+    int capacity;
+
+    // key -> Node*
+    unordered_map<int, Node*> cache;
+
+    // dummy nodes
+    // so when we insert a new node, it will be between left and right
+    Node* left;
+    Node* right;
+
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+
+        left = new Node(-1, -1);
+        right = new Node(-1, -1);
+
+        left->next = right;
+        right->prev = left;
+    }
+    
+    // When we get a value, we need to put it to the end of doubly linked list
+    // to mark it as most recently used, then finally we can return its value assuming it exists
+    int get(int key) {
+        if (cache.find(key) == cache.end()) 
+            return -1;
+
+        Node* node = cache[key];
+        remove(node);
+        add(node);
+
+        return node->val;
+    }
+    
+    // if key already exists, remove the node and add back the new node with the given value
+    // because this is outside of the if statement, we create the node regardless if it exists or not without issue
+    void put(int key, int value) {
+        if (cache.find(key) != cache.end()) 
+        {
+            Node* old_node = cache[key];
+            remove(old_node);
+        }
+
+        Node* node = new Node(key, value);
+        cache[key] = node; // overwrite
+        add(node);
+
+        // handle capacity exceeded
+        if (cache.size() > capacity) 
+        {
+            Node* delete_node = left->next;
+            remove(delete_node);
+            cache.erase(delete_node->key);
+        }
+    }
+
+private:
+    // for removing node from doubly linked list
+    // for ex, if we have 1 -> 2 -> 3
+    // 1 need to point to 3 and 3 points to 1 assuming we want to remove 2
+    void remove(Node* node) 
+    {
+        node->prev->next = node->next;
+        node->next->prev = node->prev;
+    }
+
+    // for adding node to doubly linked list
+    // add to end of the list since we can define that as most recently used
+    void add(Node* node) 
+    {
+        Node* prev_end = right->prev;
+
+        prev_end->next = node;
+        node->prev = prev_end;
+
+        node->next = right;
+        right->prev = node;
+    }
+
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */

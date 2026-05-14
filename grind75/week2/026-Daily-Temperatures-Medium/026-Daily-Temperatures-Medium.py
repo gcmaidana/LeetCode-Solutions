3bfe60712c5a3de16a585e135964d9a6e534a4ad
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-05-13 20:12:55 -0500

    Update 026-Daily-Temperatures-Medium.py

--- grind75/week2/026-Daily-Temperatures-Medium/026-Daily-Temperatures-Medium.py
+++ grind75/week2/026-Daily-Temperatures-Medium/026-Daily-Temperatures-Medium.py
@@ -1,1 +1,15 @@
 # link: https://leetcode.com/problems/daily-temperatures/
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        my_stack = []
+        ans = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+
+            while my_stack and temperatures[i] > temperatures[my_stack[-1]]:
+                idx = my_stack.pop()
+                ans[idx] = i - idx
+            my_stack.append(i)
+            
+        return ans
+        

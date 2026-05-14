2c1507df333bb642e6b6c0b9d5a63dc0eadbe15b
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-05-13 20:13:20 -0500

    Update 026-Daily-Temperatures-Medium.cpp

--- grind75/week2/026-Daily-Temperatures-Medium/026-Daily-Temperatures-Medium.cpp
+++ grind75/week2/026-Daily-Temperatures-Medium/026-Daily-Temperatures-Medium.cpp
@@ -1,1 +1,21 @@
 // link: https://leetcode.com/problems/daily-temperatures/
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> my_stack;
+        vector<int> ans(temperatures.size(), 0);
+
+        for (int i = 0; i < temperatures.size(); i++) 
+        {
+            while (!my_stack.empty() && temperatures[i] > temperatures[my_stack.top()]) 
+            {
+                int idx = my_stack.top();
+                my_stack.pop();
+                ans[idx] = i - idx;
+            }
+            my_stack.push(i);
+        }
+
+        return ans;
+    }
+};

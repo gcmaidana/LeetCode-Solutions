f494580f944f19511d9315858ba4ee3120da1195
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-08-19 19:18:09 -0500

    Update 028-Gas-Station-Medium.cpp

--- grind75/week2/028-Gas-Station-Medium/028-Gas-Station-Medium.cpp
+++ grind75/week2/028-Gas-Station-Medium/028-Gas-Station-Medium.cpp
@@ -4,10 +4,6 @@ public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
 
         // edge case: check if no solution exists, e.g. is our total gas available < cost
-        // If there exists a solution, it is guaranteed to be unique.
-        // If this were not the case with this problem, technically we would want to check:
-        // total gas available < cost, since we could have gas available == cost, which produces multiple
-        // solutions
         if (accumulate(gas.begin(), gas.end(), 0) 
             < accumulate(cost.begin(), cost.end(), 0)) 
         {

c4ffef4cf5ecfde7c5d51f90b0fbc0e85c5f580b
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-08-19 19:18:00 -0500

    Update 028-Gas-Station-Medium.py

--- grind75/week2/028-Gas-Station-Medium/028-Gas-Station-Medium.py
+++ grind75/week2/028-Gas-Station-Medium/028-Gas-Station-Medium.py
@@ -3,10 +3,6 @@ class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
 
         # edge case: check if no solution exists, e.g. is our total gas available < cost
-        # If there exists a solution, it is guaranteed to be unique.
-        # If this were not the case with this problem, technically we would want to check:
-        # total gas available < cost, since we could have gas avaiable == cost, which produces multiple
-        # solutions
         if sum(gas) < sum(cost):
             return -1
 

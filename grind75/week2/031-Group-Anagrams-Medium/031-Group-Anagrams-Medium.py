b7c00ec4d694762a8ff7042fee6f8f4f1fe9a071
Gean Maidana <51893451+gcmaidana@users.noreply.github.com>
2026-09-11 17:19:42 -0500

    Update 031-Group-Anagrams-Medium.py

--- grind75/week2/031-Group-Anagrams-Medium/031-Group-Anagrams-Medium.py
+++ grind75/week2/031-Group-Anagrams-Medium/031-Group-Anagrams-Medium.py
@@ -1,1 +1,23 @@
 # link: https://leetcode.com/problems/group-anagrams/
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        # notes
+        # Output appears to be An array of array of strings
+
+        # hashmap where the key is the freq count of each letter, the value is the word
+        # so group values with the same keys
+        anagramMap = defaultdict(list)
+        
+        for s in strs:
+            count = [0] * 26
+            for char in s:
+                count[ord(char) - ord('a')] += 1
+
+            key = tuple(count) # lists cannot be keys so count must be a tuple
+            anagramMap[key].append(s)
+        
+        
+        return list(anagramMap.values()) # already grouped, but we must return a list of lists
+                
